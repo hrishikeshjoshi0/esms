da51d3b89d43f7b2807541fedbbbb3ed5711b9e7
hrishi2323 <hrishi2323@gmail.com>
2013-01-10 00:02:22 +0530

    Added domain classes for Party and Party_Group.

---
+++ esms/grails-app/domain/com/esms/model/party/Party.groovy
@@ -0,0 +1,31 @@
+package com.esms.model.party
+
+class Party {
+	
+	Long partyId
+	String externalId
+	String description
+	String partyType
+	String status
+	String relationshipType
+
+    static constraints = {
+		description maxSize: 1000
+		partyType inList: ["PERSON", "PARTY_GROUP"]
+		status inList: ["PARTY_ENABLED", "PARTY_DISABLED"]
+		relationshipType inList: ["LEAD_REL", "CUSTOMER_REL", "EMPLOYEE", "CONTACT_REL"]
+    }
+	
+	static mapping = {
+		table 'PARTY'
+		externalId column: 'EXTERNAL_ID'
+		description column: 'DESCRIPTION'
+		partyType column: 'PARTY_TYPE'
+		status column: 'STATUS'
+		relationshipType column: 'RELATIONSHIP_TYPE'
+		
+		version false
+		id column: 'PARTY_ID'
+		tablePerHierarchy false
+	}
+}
---
+++ esms/grails-app/domain/com/esms/model/party/PartyGroup.groovy
@@ -0,0 +1,21 @@
+package com.esms.model.party
+
+class PartyGroup extends Party {
+	
+	String groupName
+	String comments
+	
+    static mapping = {
+		table 'PARTY_GROUP'
+		groupName column: 'GROUP_NAME'
+		comments column: 'COMMENTS'
+		
+		id column: 'PARTY_ID'
+    }
+	
+	static constraints = {
+		comments maxSize: 1000
+		groupName blank: false
+	}
+	
+}
