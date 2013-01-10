760aadcca022799693d98a64635bf2084eb4042b
hrishi2323 <hrishi2323@gmail.com>
2013-01-10 09:10:32 +0530

    Address Domain class.

---
+++ esms/grails-app/domain/com/esms/model/party/Address.groovy
@@ -0,0 +1,42 @@
+package com.esms.model.party
+
+class Address {
+	
+	private int addressBookId;
+	
+	private String toName;
+	
+	private String attnName;
+	
+	private String buildingName;
+	
+	private String address1;
+	
+	private String address2;
+	
+	private String directions;
+	
+	private String city;
+	
+	private String postalCode;
+	
+	private String postalCodeExt;
+	
+	private String country;
+	
+	private String stateProvince;
+	
+	private String addressType;
+
+    static constraints = {
+    }
+	
+	static mapping = {
+		table 'PARTY_ADDRESS'
+
+						
+		version false
+		id column: 'ADDRESS_BOOK_ID'
+		tablePerHierarchy false
+	}
+}
