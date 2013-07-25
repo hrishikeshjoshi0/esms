f3053b4abc1259793f67352db961b6df1b047e0b
openappengine <Sumedh@Sumedh0909>
2013-07-25 23:55:40 +0530

    Added filter

---
+++ esms/grails-app/conf/esms/DefaultFilters.groovy
@@ -0,0 +1,27 @@
+package esms
+
+import com.esms.model.order.Order
+
+class DefaultFilters {
+
+    def filters = {
+        all(controller:'*', action:'*') {
+            before = {
+				System.out.println(session["lastLoginDate"])
+				
+				def cal = Calendar.getInstance()
+				cal.set(Calendar.MONTH, cal.get(Calendar.MONTH)+1)
+				def currentDatePlusOneMonth = cal.getTime()
+				
+				def orders = Order.findAllByContractToDateLessThanEquals(currentDatePlusOneMonth)
+				session["expiringContracts"] = orders
+            }
+            after = { Map model ->
+
+            }
+            afterView = { Exception e ->
+
+            }
+        }
+    }
+}
